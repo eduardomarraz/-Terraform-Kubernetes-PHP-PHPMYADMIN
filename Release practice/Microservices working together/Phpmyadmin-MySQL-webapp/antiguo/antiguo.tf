7941d8ada5f52f2ca26fa1a2cbe12bae8f789484
eduardomr_ <eduardomr_@hotmail.com>
2024-07-15 21:00:24 +0200

    Update webapp

--- Release practice/Microservices working together/Phpmyadmin-MySQL-webapp/antiguo/antiguo.tf
+++ Release practice/Microservices working together/Phpmyadmin-MySQL-webapp/antiguo/antiguo.tf
@@ -265,6 +265,7 @@ resource "kubernetes_deployment" "webapp" {
   depends_on = [null_resource.build_docker_image]
 }
 
+
 # Service para la aplicación web
 resource "kubernetes_service" "webapp" {
   metadata {
@@ -286,11 +287,119 @@ resource "kubernetes_service" "webapp" {
   }
 }
 
+# Archivo Dockerfile
+data "template_file" "dockerfile" {
+  template = <<-EOT
+    FROM php:8.2-apache
+
+    RUN a2enmod rewrite
+
+    RUN apt-get update \\
+        && apt-get install -y libpq-dev \\
+        && docker-php-ext-install pdo pdo_pgsql
+
+    RUN docker-php-ext-install mysqli
+
+    WORKDIR /var/www/html
+
+    COPY ./html .
+
+    # Instalar Composer
+    COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+    # Instalar dependencias del proyecto
+    RUN composer install --no-dev --prefer-dist --optimize-autoloader || true
+
+    EXPOSE 80
+  EOT
+}
+
 # Construir la imagen Docker
 resource "null_resource" "build_docker_image" {
   provisioner "local-exec" {
-    command = "eval $(minikube docker-env) && docker build -t php-webserver:latest ."
+    command = <<EOT
+      eval $(minikube docker-env) && echo '${data.template_file.dockerfile.rendered}' | docker build -t php-webserver:latest -
+    EOT
   }
 
-  # No se necesita depends_on aquí, ya que el comando se ejecuta localmente
+  depends_on = [data.template_file.dockerfile]
+}
+
+
+# ConfigMap para archivos de la aplicación web
+resource "kubernetes_config_map" "webapp-config" {
+  metadata {
+    name = "webapp-config"
+  }
+
+  data = {
+    "index.html" = file("${path.module}/html/index.html")
+  }
+}
+
+resource "kubernetes_config_map" "webapp-submit" {
+  metadata {
+    name = "webapp-submit"
+  }
+
+  data = {
+    "submit.php" = file("${path.module}/html/submit.php")
+  }
+}
+
+resource "kubernetes_config_map" "webapp-vendor" {
+  metadata {
+    name = "webapp-vendor"
+  }
+
+  data = {
+    "autoload.php" = file("${path.module}/html/vendor/autoload.php")
+  }
+}
+
+# Deployment para terraform-example
+resource "kubernetes_deployment" "example" {
+  metadata {
+    name = "terraform-example"
+    labels = {
+      test = "MyExampleApp"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        test = "MyExampleApp"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "MyExampleApp"
+        }
+      }
+
+      spec {
+        container {
+          image = "miimagen:0.0.2"
+          name  = "example"
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
 }
